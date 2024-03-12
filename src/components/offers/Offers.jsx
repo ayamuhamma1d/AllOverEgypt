64c55675e11988a76b45cd4ea2a730fada761ff7
Aya Muhammad Mansour <aya676670@gmail.com>
2024-03-12 21:50:06 +0200

    add offers sec

--- src/components/offers/Offers.jsx
+++ src/components/offers/Offers.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Pagination, Autoplay } from "swiper";
@@ -7,57 +6,91 @@ import { FaRegClock } from "react-icons/fa6";
 import { IoLocationOutline } from "react-icons/io5";
 import { db } from '../config/firebase';
 import { collection, getDocs } from "firebase/firestore";
+import Nav from 'react-bootstrap/Nav';
 
 import "swiper/swiper.min.css";
 import 'swiper/css/pagination';
+import MySwiper from "../slider/MySwiper";
+import "./offer.css"
 SwiperCore.use([Pagination, Autoplay]);
 
 const Offers = () => {
+  const [content, setContent] = useState(<MySwiper tripType='StandardFourPackages' />);
+
+  useEffect(() => {
+    setContent(<MySwiper tripType='StandardFourPackages' />);
+  }, []);
+
+  const handleClick = (eventKey) => {
+    switch (eventKey) {
+      case "EgyptTour":
+        setContent(<MySwiper tripType='StandardFourPackages' />);
+        console.log('EgyptTour');
+        break;
+      case "DayTour":
+        setContent(<MySwiper tripType='LuxorDayTours' />);
+        break;
+      case "NileCruise":
+        setContent(<MySwiper tripType='nileCruise' />);
+        break;
+      default:
+        setContent(<MySwiper tripType='StandardFourPackages' />);
+    }
+  };
+
   return (
+    <>
     <div>
-    <Swiper
-        slidesPerView={3} 
-        spaceBetween={15}
-   
+                      <Nav fill variant="tabs" defaultActiveKey="EgyptTour" className=" rounded-3 p-0 m-0 bg-white shadow" onSelect={handleClick} >
+
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={0}
+        fill
+        variant="tabs"
+        defaultActiveKey="EgyptTour"
+        onSelect={handleClick}
         modules={[Pagination]}
-        className="mySwiper bg-white p-3 rounded-3 "
+        className="bg-transparent  p-0 rounded-3 m-0 w-75 m-auto"
         breakpoints={{
-            0: {
-              slidesPerView: 1.6,
-              spaceBetween:8,
+          0: {
+            slidesPerView: 2,
+            spaceBetween: 2,
+          },
+          768: {
+            slidesPerView: 2.5,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+          1200: {
+            slidesPerView: 3,
+          },
+        }}
+      >
+        <SwiperSlide  className="" >
+          <Nav.Item className="p-0 m-0 bg-white">
+          <Nav.Link eventKey="EgyptTour"  selectedType="StandardFourPackages"  className="text-black nav-text text-muted  rounded-3 py-3" >Egypt Tour Package</Nav.Link>
+          </Nav.Item>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Nav.Item>
+            <Nav.Link eventKey="DayTour" className="text-black nav-text text-muted   rounded-3 py-3" selectedType="DayTour">Egypt Day Tours</Nav.Link>
+          </Nav.Item>
+        </SwiperSlide>
+        <SwiperSlide >
+          <Nav.Item>
+            <Nav.Link eventKey="NileCruise" className="text-black nav-text text-muted  rounded-3 py-3"selectedType="nileCruise">Egypt Nile Cruise</Nav.Link>
+          </Nav.Item>
+        </SwiperSlide>
+      </Swiper>
+      </Nav>
+    </div>
+    <div className="">
+    {content}
+  </div>
+</>
+  );
+};
 
-            },
-            768: {
-              slidesPerView: 1.5,
-            },
-            975:{
-                slidesPerView: 2.2,
-
-            },
-            1024: {
-                slidesPerView: 2.5,
-              },
-              1200: {
-              slidesPerView:3,
-            },
-          
-          }}
-    >
-
-            <SwiperSlide className=" secondary-color-text" >
-            Egypt Tour Package
-            </SwiperSlide>
-            <SwiperSlide >
-            EgyptDay Tours
-            </SwiperSlide>
-            <SwiperSlide >
-            Egypt Nile Cruise
-            </SwiperSlide>
-       
-     
-    </Swiper>
-</div>
-  )
-}
-
-export default Offers
+export default Offers;
--- src/components/slider/MySwiper.jsx
+++ src/components/slider/MySwiper.jsx
@@ -68,7 +68,7 @@ const MySwiper = (props) => {
             spaceBetween:20
           },
           500:{
-            slidesPerView: 2.1,
+            slidesPerView: 2.6,
             spaceBetween:10
           },
           768: {
@@ -79,7 +79,7 @@ const MySwiper = (props) => {
             slidesPerView: 3.5,
           },
           1300: {
-            slidesPerView: 4,
+            slidesPerView: 5.5,
           },
         }}
         className="mySwiper"
@@ -97,11 +97,11 @@ const MySwiper = (props) => {
                 </Link>
                 <div className="p-3">
                   <Link to={`/trips/${trip.id}`}>
-                    <h6 className="text-start fw-bold lh-base fst-italic">
+                    <p className="text-start text-white fw-bold lh-base fst-italic">
                       {trip.tripTitle.split(" ").slice(0, 8).join(" ") +"..."}
-                    </h6>
+                    </p>
                   </Link>
-                  <p className="text-start fst-italic mb-3">
+                  {/* <p className="text-start fst-italic mb-3">
                     {trip.overview.split(" ").slice(0, 10).join(" ")+"..."}
                   </p>
                   <hr className="text__color" />
@@ -139,7 +139,7 @@ const MySwiper = (props) => {
                       </div>
                     </div>
 
-                  </div>
+                  </div> */}
                 </div>
               </div>
             </div>
@@ -151,3 +151,154 @@ const MySwiper = (props) => {
 };
 
 export default MySwiper;
+// import React, { useEffect, useState } from "react";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import SwiperCore, { Pagination, Autoplay } from "swiper";
+// import { Link } from "react-router-dom";
+// import { FaRegClock } from "react-icons/fa6";
+// import { IoLocationOutline } from "react-icons/io5";
+// import { db } from '../config/firebase';
+// import { collection, getDocs } from "firebase/firestore";
+// import "./Myswiper.css";
+
+// import "swiper/swiper.min.css";
+// import 'swiper/css/pagination';
+
+// SwiperCore.use([Pagination, Autoplay]);
+
+// const MySwiper = (props) => {
+//   const [tripData, setTripData] = useState([]);
+
+//   console.log('====================================');
+//   console.log(props.tripType);
+//   console.log('====================================');
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const querySnapshot = await getDocs(collection(db, "trips"));
+//         const trips = querySnapshot.docs.map((doc) => ({
+//           id: doc.id,
+//           ...doc.data()
+//         }));
+//         let filteredTrips = trips;
+
+//         if (props.tripType != undefined && props.tripType) {
+//           filteredTrips = filteredTrips.filter((trip) => trip.type.includes(props.tripType));
+//           filteredTrips = filteredTrips.sort((a, b) => parseInt(a.duration) - parseInt(b.duration));
+//           setTripData(filteredTrips);
+//         }
+//         else {
+//           filteredTrips = filteredTrips.sort((a, b) => parseInt(a.duration) - parseInt(b.duration));
+//           setTripData(filteredTrips);
+ 
+
+//         }
+
+
+
+//       } catch (error) {
+//         console.error("Error fetching data: ", error);
+//       }
+//     };
+//     fetchData();
+//   }, [props.tripType]);
+
+//   return (
+//     <div className="swiper-container">
+//       <Swiper
+//         slidesPerView={4}
+//         spaceBetween={10}
+    
+//         autoplay={{
+//           delay: 3000,
+//           disableOnInteraction: false,
+//         }}
+//         breakpoints={{
+//           0: {
+//             slidesPerView: 1.7,
+//             spaceBetween:20
+//           },
+//           500:{
+//             slidesPerView: 2.1,
+//             spaceBetween:10
+//           },
+//           768: {
+//             slidesPerView: 2.8,
+//             spaceBetween:10
+//           },
+//           1024: {
+//             slidesPerView: 5,
+//           },
+//           1300: {
+//             slidesPerView: 5,
+//           },
+//         }}
+//         className="mySwiper"
+//       >
+//         {tripData.map((trip) => (
+//           <SwiperSlide key={trip.id}>
+//             <div className="swiper-slide-container">
+//               <div className=" rounded-top-3 rounded-bottom mb-5 slider_slide slider_slides ">
+//                 <Link to={`/trips/${trip.id}`} className="flex-grow-1">
+//                   <img
+//                     src={trip.image}
+//                     alt={trip.tripTitle}
+//                     className="w-full  slider-img rounded-top-3 slider-image"
+//                   />
+//                 </Link>
+//                 <div className="">
+//                   <Link to={`/trips/${trip.id}`}>
+//                     <h6 className="text-start bg-white shadow fw-bold lh-base fst-italic">
+//                       {trip.tripTitle.split(" ").slice(0, 8).join(" ") +"..."}
+//                     </h6>
+//                   </Link>
+//                   {/* <p className="text-start fst-italic mb-3">
+//                     {trip.overview.split(" ").slice(0, 10).join(" ")+"..."}
+//                   </p>
+//                   <hr className="text__color" /> */}
+//                   {/* <div className="d-flex align-items-center flex-wrap justify-content-between ">
+//                     <div className="card-width-1">
+//                       <div className="location text-start text-dark">
+//                         <Link className="text-decoration-none text-black d-flex align-items-center mb-2" to={{
+//                           pathname: '/filter',
+//                           search: `?destination=${trip.destination[0]}`
+//                         }}>
+//                           <IoLocationOutline className="me-1 text-beige" />{" "}
+//                           <span className="fs-6 fst-italic">{trip.destination[0]}</span>
+//                         </Link>
+//                       </div>
+//                     </div>
+//                     <div className="card-width-2">
+//                       {trip && trip.pricePackages && trip.pricePackages.length > 0 && (
+//                         <h5 className="text-slate-900 fw-bold mb-0 text-start">
+//                           <span className="text-lg text-beige">$ </span> {
+//                             Math.min(
+//                               ...trip.pricePackages.flatMap(pricePackage =>
+//                                 pricePackage.options.map(option => option.price)
+//                               )
+//                             )
+//                           }
+//                         </h5>
+//                       )}
+//                     </div>
+//                     <div className="card-width-3">
+//                       <div className="day text-start">
+//                         <Link className="text-decoration-none text-black d-flex align-items-center mb-2">
+//                           <FaRegClock className="me-2 text-beige fs-6" />{" "}
+//                           <span>{trip.duration} Days</span>
+//                         </Link>
+//                       </div>
+//                     </div>
+
+//                   </div> */}
+//                 </div>
+//               </div>
+//             </div>
+//           </SwiperSlide>
+//         ))}
+//       </Swiper>
+//     </div>
+//   );
+// };
+
+// export default MySwiper;
